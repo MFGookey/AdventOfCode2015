5ae3e8ffb73037d0888640953edd8645fefa9945
MFGookey <MFGookey@users.noreply.github.com>
2023-11-27 13:10:42 -0500

    Day 3 part 2

--- 2015-12-03/CaretNavigation/CaretNavigation.Cmd/Program.cs
+++ 2015-12-03/CaretNavigation/CaretNavigation.Cmd/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CaretNavigation.Core;
 using Common.Utilities.IO;
+using Common.Utilities.TwoD;
 
 namespace CaretNavigation.Cmd
 {
@@ -28,6 +31,32 @@ namespace CaretNavigation.Cmd
 
       var navigator = new Navigator(reader.ReadFile(filePath));
       Console.WriteLine(navigator.GetDistinctPoints());
+
+      var testOddNavigator = new Navigator(new string("^v".ToCharArray().Where((c, index) => { return index % 2 == 0; }).ToArray<char>()));
+      var testEvenNavigator = new Navigator(new string("^v".ToCharArray().Where((c, index) => { return index % 2 == 1; }).ToArray<char>()));
+      foreach (var point in testOddNavigator.GetRoutePoints())
+      {
+        Console.WriteLine(point);
+      }
+      Console.WriteLine();
+
+
+      foreach (var point in testEvenNavigator.GetRoutePoints())
+      {
+        Console.WriteLine(point);
+      }
+
+      var testVisitedPoints = new List<Point>(testOddNavigator.GetRoutePoints());
+      testVisitedPoints.AddRange(testEvenNavigator.GetRoutePoints());
+      Console.WriteLine(testVisitedPoints.Distinct().Count());
+
+      var oddNavigator = new Navigator(new string(reader.ReadFile(filePath).ToCharArray().Where((c, index) => { return index % 2 == 0; }).ToArray<char>()));
+      var evenNavigator = new Navigator(new string(reader.ReadFile(filePath).ToCharArray().Where((c, index) => { return index % 2 == 1; }).ToArray<char>()));
+
+      var visitedPoints = new List<Point>(oddNavigator.GetRoutePoints());
+      visitedPoints.AddRange(evenNavigator.GetRoutePoints());
+      Console.WriteLine(visitedPoints.Distinct().Count());
+
       _ = Console.ReadLine();
     }
   }
