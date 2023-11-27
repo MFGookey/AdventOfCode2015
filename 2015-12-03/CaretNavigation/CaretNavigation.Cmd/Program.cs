264f0bb663db29701218ee698b0e2e0a45befdb1
MFGookey <MFGookey@users.noreply.github.com>
2023-11-27 12:56:10 -0500

    Day 3 part 1

---
+++ 2015-12-03/CaretNavigation/CaretNavigation.Cmd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CaretNavigation.Core;
+using Common.Utilities.IO;
+
+namespace CaretNavigation.Cmd
+{
+  /// <summary>
+  /// The entrypoint for CaretNavigation.Cmd
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// CaretNavigation.Cmd entry point
+    /// </summary>
+    /// <param name="args">Command line arguments (not used)</param>
+    static void Main(string[] args)
+    {
+      var filePath = "./input";
+      var reader = new FileReader();
+
+      var testNavigator = new Navigator("^>v<");
+      foreach(var point in testNavigator.GetRoutePoints())
+      {
+        Console.WriteLine(point);
+      }
+
+      Console.WriteLine(testNavigator.GetDistinctPoints());
+
+      var navigator = new Navigator(reader.ReadFile(filePath));
+      Console.WriteLine(navigator.GetDistinctPoints());
+      _ = Console.ReadLine();
+    }
+  }
+}
