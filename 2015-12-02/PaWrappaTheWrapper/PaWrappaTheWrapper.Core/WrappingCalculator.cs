304c6dd712cd883a56b94f50ea6b813f40eb2f2b
MFGookey <MFGookey@users.noreply.github.com>
2023-11-27 12:21:37 -0500

    Day 2 part 1

---
+++ 2015-12-02/PaWrappaTheWrapper/PaWrappaTheWrapper.Core/WrappingCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaWrappaTheWrapper.Core
+{
+  public class WrappingCalculator
+  {
+    private readonly ICollection<Gift> _gifts;
+
+    public WrappingCalculator(IEnumerable<string> giftDimensions)
+    {
+      _gifts = new List<Gift>();
+      foreach(string gift in giftDimensions)
+      {
+        _gifts.Add(new Gift(gift));
+      }
+    }
+
+    public long GetTotalWrappingArea()
+    {
+      long result = 0;
+      foreach (var gift in _gifts)
+      {
+        result += gift.GetWrappingSize();
+      }
+
+      return result;
+    }
+  }
+}
