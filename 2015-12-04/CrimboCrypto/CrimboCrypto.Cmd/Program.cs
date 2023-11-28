990c708e509f66cad8e2b7af11682fe85053d129
MFGookey <MFGookey@users.noreply.github.com>
2023-11-28 10:36:10 -0500

    Day 4 Part 1

---
+++ 2015-12-04/CrimboCrypto/CrimboCrypto.Cmd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Common.Utilities.IO;
+using CrimboCrypto.Core;
+
+namespace CrimboCrypto.Cmd
+{
+  /// <summary>
+  /// The entrypoint for CrimboCrypto.Cmd
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// CrimboCrypto.Cmd entry point
+    /// </summary>
+    /// <param name="args">Command line arguments (not used)</param>
+    static void Main(string[] args)
+    {
+      string testSeed = "abcdef";
+      var miner = new HashMiner(testSeed);
+      Console.WriteLine(miner.GetSeedModifier(miner.HashPrefixEvaluator("00000")));
+      string seed = "yzbqklnj";
+      miner = new HashMiner(seed);
+      Console.WriteLine(miner.GetSeedModifier(miner.HashPrefixEvaluator("00000")));
+      _ = Console.ReadLine();
+    }
+  }
+}
---
+++ 2015-12-04/CrimboCrypto/CrimboCrypto.Core/HashMiner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace CrimboCrypto.Core
+{
+  public class HashMiner
+  {
+    private string _seed;
+    private MD5CryptoServiceProvider _hashProvider;
+    public HashMiner(string seed)
+    {
+      _hashProvider = new MD5CryptoServiceProvider();
+      _hashProvider.Initialize();
+      _seed = seed;
+    }
+
+    public int GetSeedModifier(Func<string, bool> evaluator)
+    {
+      int testValue = 0;
+      string hash = string.Empty;
+      do
+      {
+        testValue++;
+        hash = this.ByteArrayToString(_hashProvider.ComputeHash(Encoding.ASCII.GetBytes($"{_seed}{testValue}")));
+      } while (!evaluator(hash));
+      return testValue;
+    }
+
+    public Func<string, bool> HashPrefixEvaluator(string desiredPrefix)
+    {
+      return (string hash) => { return hash.StartsWith(desiredPrefix, StringComparison.Ordinal); };
+    }
+
+    private string ByteArrayToString(byte[] ba)
+    {
+      StringBuilder hex = new StringBuilder(ba.Length * 2);
+      foreach (byte b in ba)
+        hex.AppendFormat("{0:x2}", b);
+      return hex.ToString();
+    }
+  }
+}
